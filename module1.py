33de8ce2f7502b3a9596d5e0fe32e08c3b81fb60
saipavans <sp403250@dal.ca>
2018-01-22 15:20:57 -0400

    second commit

--- module1.py
+++ module1.py
@@ -1,7 +1,7 @@
 def divisibility_by7(a):
 	if x%7==0:		
-		print("the number is divisible by 7")
+		print("is divisible by 7")
 	else:	
-		print("it is not divisible by")
+		print(" is not divisible by")
 
 divisibility_by7(14)
